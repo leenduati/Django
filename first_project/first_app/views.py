2e050a38ac0dc3fca17bf025f64bcbdb2300cf2d
leenduati <leenduati21@gmail.com>
2022-05-27 16:07:15 +0300

    Added first app in Django that displayed hello.world

---
+++ first_project/first_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Imported HTTPREsponse Object from Django HTTP MOdule
+
+# Create your views here.
+# Each view for this app is going to exist within that views.py file as its own function.
+
+# We just created one view called Index, and each view is also going to take in at least one argument
+# The HttpObject is called request from django.http
+# Each view must return a httpresponse object
+
+def index(request):
+    return HttpResponse("Hello World")
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
 
 urlpatterns = [
+    path('', views.index, name="hello"),
     path('admin/', admin.site.urls),
 ]
